9a82ff0a82f069997060611d2c162f85b33e1198
apernia <apernia@conexum.co>
2023-11-13 09:14:32 -0300

    Modified routes on main nav

--- coder-next-app/app/components/ui/MenuList.js
+++ coder-next-app/app/components/ui/MenuList.js
@@ -4,6 +4,8 @@ import Link from "next/link"
 
 const MenuList = ({open, setOpen}) => {
 
+    const handleCLose = () => setOpen(false)
+
     return (
             <div className={`${open ? 'visible opacity-100' : 'invisible opacity-0'} transition-all fixed inset-0 bg-black/75 flex justify-end`}>
                 <aside className={`${!open ? 'translate-x-48' : ''} transition-all w-48 bg-gray-950`}>
@@ -14,9 +16,8 @@ const MenuList = ({open, setOpen}) => {
                         X
                     </div>
                     <nav className="flex flex-col gap-4 px-4 text-white">
-                        <Link href={"#"}>Enlace 1</Link>
-                        <Link href={"#"}>Enlace 2</Link>
-                        <Link href={"#"}>Enlace 3</Link>
+                        <Link onClick={handleCLose} href={"/"}>Inicio</Link>
+                        <Link onClick={handleCLose} href={"/contact"}>Contacto</Link>
                     </nav>
                 </aside>
             </div>
